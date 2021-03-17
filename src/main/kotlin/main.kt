6002e617a78cb9aa73c1cb3e070baaeb98323e8e
lobis <luis.antonio.obis@gmail.com>
2021-03-17 21:10:16 +0100

    idea: using enum for variables?

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,37 +3,45 @@ import space.kscience.gdml.*
 import java.io.File
 import kotlin.io.*
 
-// geometry variables
-val worldSize = 1000
-
-val chamber = mapOf<String, Double>(
-    "height" to 30.0, "diameter" to 102.0,
-    "outerSquareSide" to 134.0, "backplateThickness" to 15.0,
-    "teflonWallThickness" to 11.0, "readoutKaptonThickness" to 0.5,
-    "readoutCopperThickness" to 0.2, "readoutPlaneSide" to 60.0
-)
+enum class Chamber(val mm: Double) {
+    Diameter(102.0),
+}
 
 val geometry = Gdml {
-    loadMaterialsFromUrl("https://raw.githubusercontent.com/rest-for-physics/materials/main/NIST.xml")
+    // geometric variables
+    val worldSize = 1000
+    val chamber = mapOf<String, Double>(
+        "height" to 30.0, "diameter" to 102.0,
+        "outerSquareSide" to 134.0, "backplateThickness" to 15.0,
+        "teflonWallThickness" to 11.0, "readoutKaptonThickness" to 0.5,
+        "readoutCopperThickness" to 0.2, "readoutPlaneSide" to 60.0
+    )
 
+    // materials
+    loadMaterialsFromUrl("https://raw.githubusercontent.com/rest-for-physics/materials/main/NIST.xml")
     val materialsMap = mapOf(
         "Copper" to materials.composite("G4_Cu"),
         "Lead" to materials.composite("G4_Pb"),
+        "Teflon" to materials.composite("G4_TEFLON"),
     )
 
     structure {
+        // world
         val worldMaterial = materials.composite("G4_AIR")
         val worldBox = solids.box(worldSize, worldSize, worldSize, "worldBox")
+        // chamber
 
-        val chamberSolidBase =
+        val chamberBodySolid = solids.subtraction(
             solids.box(
                 chamber["outerSquareSide"]!!,
                 chamber["outerSquareSide"]!!,
                 chamber["height"]!!,
-                "chamberSolidBase"
-            )
-        val chamberSolidHole = solids.tube(chamber["diameter"]!! * 0.5, chamber["height"]!!, "chamberSolidHole")
-        val chamberSolid = solids.subtraction(chamberSolidBase, chamberSolidHole, "chamberSolid")
+                "chamberBodyBaseSolid"
+            ), solids.tube(Chamber.Diameter.mm * 0.5, chamber["height"]!!, "chamberBodyHoleSolid"),
+            "chamberBodySolid"
+        )
+        val chamberBodyVolume = volume(materialsMap["Copper"]!!, chamberBodySolid, "chamberBodyVolume")
+
         val chamberBackplateSolid = solids.box(
             chamber["outerSquareSide"]!!,
             chamber["outerSquareSide"]!!,
@@ -41,8 +49,15 @@ val geometry = Gdml {
             "chamberBackplateSolid"
         )
         val chamberBackplateVolume = volume(materialsMap["Copper"]!!, chamberBackplateSolid, "chamberBackplateVolume")
-        val chamberBodyVolume = volume(materialsMap["Copper"]!!, chamberSolid, "chamberBodyVolume")
 
+        val chamberTeflonWallSolid =
+            solids.tube(chamber["diameter"]!! * 0.5, chamber["height"]!!, "chamberTeflonWallSolid") {
+                rmin = chamber["diameter"]!! * 0.5 - chamber["teflonWallThickness"]!!
+            }
+        val chamberTeflonWallVolume =
+            volume(materialsMap["Teflon"]!!, chamberTeflonWallSolid, "chamberTeflonWallVolume")
+
+        //
         val chamberVolume = assembly {
             physVolume(chamberBackplateVolume) {
                 name = "chamberBackplate"
@@ -54,8 +69,10 @@ val geometry = Gdml {
             physVolume(chamberBodyVolume) {
                 name = "chamberBody"
             }
+            physVolume(chamberTeflonWallVolume) {
+                name = "chamberTeflonWall"
+            }
         }
-
 
         // chamber teflon walls
 
