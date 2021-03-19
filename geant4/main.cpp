c127db8bed97be67cce7ec7dea014ac9a61bc9be
lobis <luis.antonio.obis@gmail.com>
2021-03-19 18:49:39 +0100

    implemented simple Gdml Geant4 validation

---
+++ geant4/main.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+
+#include "G4GDMLParser.hh"
+
+using namespace std;
+
+void parseGdml(const string &filename = "Setup.gdml") {
+    G4GDMLParser parser;
+    cout << "Reading from '" << filename << "'" << endl;
+    parser.Read(filename.c_str());
+}
+
+int main() {
+    cout << "Starting..." << endl;
+    parseGdml();
+}
